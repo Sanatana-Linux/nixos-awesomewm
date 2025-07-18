a1ed3b762d08b26428a21c76edd34d56b747d840
Thomas Leon Highbaugh <me@thomasleonhighbaugh.me>
2025-07-18 00:43:22 -0700

    2025-07-18 00:43:22

--- ui/menu/init.lua
+++ ui/menu/init.lua
@@ -10,6 +10,7 @@ local is_supported = require("lib").is_supported
 local capi = { awesome = awesome, screen = screen, client = client }
 local screenshot = require("service.screenshot").get_default()
 local powermenu = require("ui.powermenu").get_default()
+local dpi = beautiful.xresources.apply_dpi
 
 local menu = {}
 
@@ -17,13 +18,14 @@ local function create_desktop_menu()
     return modules.menu({
         theme = {
             item_font = beautiful.font_h0,
+            item_width = dpi(250),
         },
         items = {
             {
-                label = "awesome",
+                label = "󰒓 awesome", -- nf-md-cog
                 items = {
                     {
-                        label = "config",
+                        label = "󰒓 config", -- nf-md-cog
                         exec = function()
                             local app = Gio.AppInfo.get_default_for_type(
                                 "inode/directory"
@@ -40,37 +42,13 @@ local function create_desktop_menu()
                         end,
                     },
                     {
-                        label = "set wallpaper",
-                        exec = function()
-                            awful.spawn.easy_async(
-                                "zenity --file-selection",
-                                function(stdout)
-                                    stdout = string.gsub(stdout, "\n", "")
-                                    local formats = { "png", "jpg", "jpeg" }
-                                    if
-                                        stdout ~= nil
-                                        and stdout ~= ""
-                                        and is_supported(stdout, formats)
-                                    then
-                                        for s in capi.screen do
-                                            s.wallpaper:set_image(stdout)
-                                        end
-                                        -- NOTE: This change is for the current session only.
-                                        -- To make it permanent, change `theme.wallpaper`
-                                        -- in your theme file.
-                                    end
-                                end
-                            )
-                        end,
-                    },
-                    {
-                        label = "restart",
+                        label = "󰑓 restart", -- nf-md-restart
                         exec = function()
                             capi.awesome.restart()
                         end,
                     },
                     {
-                        label = "power",
+                        label = "⏻ power", -- nf-fa-power_off
                         exec = function()
                             powermenu:toggle()
                         end,
@@ -78,22 +56,22 @@ local function create_desktop_menu()
                 },
             },
             {
-                label = "screenshot",
+                label = " screenshot", -- nf-fa-picture_o
                 items = {
                     {
-                        label = "full",
+                        label = "󰍹 full", -- nf-md-camera
                         exec = function()
                             screenshot:take_full()
                         end,
                     },
                     {
-                        label = "full 5s delay",
+                        label = "󰍹 full 5s delay", -- nf-md-camera
                         exec = function()
                             screenshot:take_delay(5)
                         end,
                     },
                     {
-                        label = "select area",
+                        label = "󰄄 select area", -- nf-fa-crop
                         exec = function()
                             screenshot:take_select()
                         end,
@@ -101,7 +79,7 @@ local function create_desktop_menu()
                 },
             },
             {
-                label = "terminal",
+                label = " terminal", -- nf-dev-terminal
                 exec = function()
                     local app = "kitty"
                     if app then
@@ -110,7 +88,7 @@ local function create_desktop_menu()
                 end,
             },
             {
-                label = "files",
+                label = " files", -- nf-fa-folder_open
                 exec = function()
                     local app = "thunar"
                     if app then
@@ -119,9 +97,9 @@ local function create_desktop_menu()
                 end,
             },
             {
-                label = "web",
+                label = " web", -- nf-fa-firefox
                 exec = function()
-                    local app = "firefox"
+                    local app = "firefox-nightly"
                     if app then
                         awful.spawn(app)
                     end
@@ -137,13 +115,13 @@ local function create_client_menu(c)
 
     for _, t in ipairs(c.screen.tags) do
         table.insert(move_to_tag_item, {
-            label = string.format("%s: %s", t.index, t.name),
+            label = string.format(" %s: %s", t.index, t.name), -- nf-oct-chevron_right
             exec = function()
                 c:move_to_tag(t)
             end,
         })
         table.insert(toggle_on_tag_item, {
-            label = string.format("%s: %s", t.index, t.name),
+            label = string.format(" %s: %s", t.index, t.name), -- nf-fa-eye
             exec = function()
                 c:toggle_tag(t)
             end,
@@ -153,51 +131,74 @@ local function create_client_menu(c)
     return modules.menu({
         auto_expand = true,
         theme = {
-            item_font = beautiful.font_h0,
+            item_font = beautiful.font_name .. "11",
+            item_width = dpi(250),
         },
         items = {
             {
-                label = "move to tag",
+                label = " move", -- nf-oct-chevron_right
                 items = move_to_tag_item,
             },
             {
-                label = "toggle on tag",
+                label = " toggle on tag", -- nf-fa-eye
                 items = toggle_on_tag_item,
             },
             not c.requests_no_titlebar and {
-                label = "toggle titlebar",
+                label = "󰐃 toggle titlebar", -- nf-md-window_restore
                 exec = function()
                     awful.titlebar.toggle(c, "top")
                 end,
             },
             {
-                label = "move to center",
+                label = "󰆧 move to center", -- nf-md-crosshairs_gps
                 exec = function()
                     awful.placement.centered(c, { honor_workarea = true })
                 end,
             },
+
             {
-                label = c.ontop and "unset ontop" or "set ontop",
+                label = (c.ontop and "󰒍 unset ontop" or "󰒍 set ontop"), -- nf-md-arrow_up_box
                 exec = function()
                     c.ontop = not c.ontop
                 end,
             },
             {
-                label = c.fullscreen and "unset fullscreen" or "set fullscreen",
+                label = (c.sticky and "󰐃 unset sticky" or "󰐃 set sticky"), -- nf-md-window_restore
+                exec = function()
+                    c.sticky = not c.sticky
+                end,
+            },
+            {
+                label = (c.above and "󰒍 unset above" or "󰒍 set above"), -- nf-md-arrow_up_box
+                exec = function()
+                    c.above = not c.above
+                end,
+            },
+            {
+                label = (c.below and "󰒎 unset below" or "󰒎 set below"), -- nf-md-arrow_down_box
+                exec = function()
+                    c.below = not c.below
+                end,
+            },
+            {
+                label = (
+                    c.fullscreen and "󰖳 unset fullscreen"
+                    or "󰖳 set fullscreen"
+                ), -- nf-md-fullscreen
                 exec = function()
                     c.fullscreen = not c.fullscreen
                     c:activate()
                 end,
             },
             {
-                label = c.maximized and "unmaximize" or "maximize",
+                label = (c.maximized and "󰁌 unmaximize" or "󰁌 maximize"), -- nf-md-arrow_expand
                 exec = function()
                     c.maximized = not c.maximized
                     c:activate()
                 end,
             },
             {
-                label = c.minimized and "unminimize" or "minimize",
+                label = (c.minimized and "󰘕 unminimize" or "󰘕 minimize"), -- nf-md-window_minimize
                 exec = function()
                     if c.minimized then
                         c.minimized = false
@@ -208,7 +209,7 @@ local function create_client_menu(c)
                 end,
             },
             {
-                label = "close",
+                label = " close", -- nf-fa-close
                 exec = function()
                     c:kill()
                 end,
